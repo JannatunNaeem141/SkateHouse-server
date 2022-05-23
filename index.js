c23f8c6fd720abe45d72f0667a008900b2181682
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-05-23 18:42:52 +0600

    api created for find one by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,12 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
     }
     finally {
 
